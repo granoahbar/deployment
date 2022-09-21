229c8c8e0f5460945b1157b9c4d1b757509d0b9b
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-09-21 14:44:53 -0600

    updated

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const path = require('path')
 
 require('dotenv').config()
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/', express.static(path.join(__dirname, '../index.html')))
+
+app.post('/register', (req, res) => res.status(200).send('Registered'))
+
+app.listen(port, () => {
+    console.log('Docked at port ' + port)
+  })
