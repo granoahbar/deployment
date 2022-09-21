dc68cbe0394a95fc73ed874ccd73736d6b559b22
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-09-21 14:53:39 -0600

    updated

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ app.use(express.json())
 
 app.use('/', express.static(path.join(__dirname, '../client/index.html')))
 
-app.post('/register', (req, res) => res.status(200).send('Registered'))
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.js'))
+  })
 
 app.listen(port, () => {
     console.log('Docked at port ' + port)
