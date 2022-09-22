f1a6bdf4673b5c34525383dcfe399e0f4e5580a2
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-09-22 15:00:34 -0600

    updated with roll bar

--- server/index.js
+++ server/index.js
@@ -9,6 +9,17 @@ const app = express()
 
 app.use(express.json())
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'dca1053ba69340078672cd065421849e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use('/', express.static(path.join(__dirname, '../client/index.html')))
 
 app.get('/', (req, res) => {
